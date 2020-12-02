14b4b2a3588f508d997db323c315b18b3c67a175
Thanh-Truong <hopchuathurac@gmail.com>
2020-12-02 13:08:44 +0100

    Day2.Parse lines

--- day2/day2.py
+++ day2/day2.py
@@ -4,9 +4,34 @@ class Password():
         self.min = None
         self.max = None
         self.letter = None
-        self.password = line
+        self.password = None
         self.str = line
+        self.parse()
+
+    def parse_min(self, str):
+        tokens = str.split("-")
+        return tokens[0]
     
+    def parse_max(self, str):
+        tokens = str.split(" ")
+        return tokens[0]
+    
+    def parse_letter(self, str):
+        tokens = str.split(":")
+        return tokens[0]
+
+    def parse(self):
+        str_min = self.parse_min(self.str)
+        str_max = self.parse_max(self.str[len(str_min)+1:])
+        str_letter = self.parse_letter(self.str[len(str_min)+1 + len(str_max)+1:])
+        self.min = int(str_min)
+        self.max = int(str_max)
+        self.letter = str_letter
+        self.password = self.str[len(str_min) +  1 + len(str_max) + 1 + len(str_letter) + 1 + 1:]
+
+    def toStr(self):
+        print("min={} max={} letter={} password={}".format(self.min, self.max, self.letter, self.password))
+
     def isValidate(self):
         return self.password
 
@@ -20,6 +45,7 @@ def main():
         count = 0
         for line in lines:
             password = Password(line)
+            password.toStr()
             if password.isValidate():
                 count = count + 1
         print(count)
