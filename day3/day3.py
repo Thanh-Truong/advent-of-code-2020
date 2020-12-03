ed3a487abe47f57d075225ff9dcc2a385c597d46
Thanh-Truong <hopchuathurac@gmail.com>
2020-12-03 10:52:51 +0100

    Day3. countTreesWithSlope

--- day3/day3.py
+++ day3/day3.py
@@ -6,23 +6,29 @@ def foo():
             print("{} {} ".format(line, len(line)))
             array.append(line)
 
-def main():
+def makeArray():
     with open('input.txt', 'r') as f:
         lines = f.read().splitlines()       
         array = []
         for line in lines:
             array.append(line)
-        countTrees = 0
-        col = 0
-        for row in range(len(array)):
-            col = row * 3
-            #print(len(array[row]))
-            checkPoint = col % len(array[row])
-            if row != 0:
-                if array[row][checkPoint] == '#':
-                    countTrees = countTrees + 1
-            #print("row = {} col = {} checkPoint = {} value = {} trees = {}".format(row, col, checkPoint, array[row][checkPoint], countTrees))
-    print(countTrees)
+    return array
+
+def countTreesWithSlope(array, right, down):
+    countTrees = 0
+    col = 0
+    for row in range(len(array)):
+        col = row * right
+        checkPoint = col % len(array[row])
+        if row != 0: # start position
+            if array[row][checkPoint] == '#':
+                countTrees = countTrees + 1
+        #print("row = {} col = {} checkPoint = {} value = {} trees = {}".format(row, col, checkPoint, array[row][checkPoint], countTrees))
+    return countTrees
+
+def main():
+    array = makeArray()
+    print(countTreesWithSlope(array, right=3, down=1))
 
 if __name__ == "__main__":
     main()
