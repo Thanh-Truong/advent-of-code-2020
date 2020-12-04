8e5cb0b6a09ab66f0eb1b8e16e059c125a71293f
Thanh-Truong <hopchuathurac@gmail.com>
2020-12-04 18:14:56 +0100

    nicer

--- day4/day4.py
+++ day4/day4.py
@@ -2,27 +2,26 @@ import re
 from functools import reduce
 
 def isValidPartTwo(passport):
-    rules = {'byr': isByrValid, 'iyr': isIyrValid, 'eyr': isEyrValid,
-             'hgt': isHgtValid, 'hcl': isHclValid, 'ecl': isEclValid, 'pid': isPidValid}
+    rules = {
+        # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+        'byr': lambda byr: 1920 <= int(byr) <= 2002,
+        # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+        'iyr': lambda iyr: 2010 <= int(iyr) <= 2020,
+        # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+        'eyr': lambda eyr: 2020 <= int(eyr) <= 2030,
+        'hgt': isHgtValid,
+        # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+        'hcl': lambda hcl: hcl and re.search("^#[0-9a-f]{6}$", hcl),
+        # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+        'ecl': lambda ecl: ecl in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
+        # pid (Passport ID) - a nine-digit number, including leading zeroes.
+        'pid': lambda pid: pid and re.search("^[0-9]{9}$", pid)}
     try:
         # apply rules
         res = map((lambda field: rules[field](passport.get(field))), rules)
         return reduce(lambda x, y: x and y, res)
     except Exception:
         return False
-    
- 
-def isByrValid(byr):
-    # byr (Birth Year) - four digits; at least 1920 and at most 2002.
-    return 1920 <= int(byr) <= 2002
-
-def isIyrValid(iyr):
-    # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-    return 2010 <= int(iyr) <= 2020
-
-def isEyrValid(eyr):
-    # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-    return 2020 <= int(eyr) <= 2030
 
 def isHgtValid(hgt):
     # hgt (Height) - a number followed by either cm or in:
@@ -39,18 +38,6 @@ def isHgtValid(hgt):
             return False
     except Exception:
         return False
-
-def isHclValid(hcl):
-    # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-    return hcl and re.search("^#[0-9a-f]{6}$", hcl)
-
-def isEclValid(ecl):
-    # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-    return ecl in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
-
-def isPidValid(pid):
-    # pid (Passport ID) - a nine-digit number, including leading zeroes.
-    return pid and re.search("^[0-9]{9}$", pid)
 
 def allRequiredFieldsPresent(passport):
     required = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
