2947100510efdb625f415fc3bc94b11cd027c6a4
Thanh-Truong <hopchuathurac@gmail.com>
2020-12-06 12:53:57 +0100

    Day6.PartTwo

--- day6/day6.py
+++ day6/day6.py
@@ -1,15 +1,34 @@
-if __name__ == "__main__":
+def groups():
     with open('input.txt', 'r') as f:
         lines = f.read().splitlines()
-        counts = []
-        dic = {}
+        group = []
         for line in lines:
             if line == "":
-                counts.append(sum(1 for i in dic.keys()))
-                dic = {}
+                yield group
+                group.clear()
             else:
-                for c in line:
-                    dic[c] = 'None'
-        counts.append(sum(1 for i in dic.keys()))
-        #print(counts)
-        print(sum(counts))
+                group.append(line)
+        if len(group) > 0:
+            yield group
+
+def countQuestionsAnyOneAnswerYes(group):
+    d = {}
+    for person in group:
+        for c in person:
+            d[c] = 'None'
+    return  sum([1 for i in d.keys()])
+
+def countQuestionsEveryOneAnswerYes(group):
+    firstPerson = group[0]
+    count = 0
+    for question in firstPerson:
+        # question was answered by all others ?
+        s = sum(map(lambda other: 1 if question in other else 0, group[1:]))
+        if s == len(group) - 1: # every other
+            count = count +  1
+    return  count
+
+if __name__ == "__main__":
+    print(sum(map(lambda group: countQuestionsAnyOneAnswerYes(group), groups())))
+    print(sum(map(lambda group: countQuestionsEveryOneAnswerYes(group), groups())))
+        
