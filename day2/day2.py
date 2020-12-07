3fc7577c6add5130c47857cc7d40d95ea9571b26
Thanh-Truong <hopchuathurac@gmail.com>
2020-12-07 23:48:42 +0100

    removed

--- day2/day2.py
+++ day2/day2.py
@@ -1,35 +1,29 @@
 
-class Policy1():    
-    def apply(self, password):
-        count = sum(map(lambda c : 1 if password.letter in c else 0, password.password))
-        return password.min <= count and password.max >= count
+def policy1(password):
+    count = sum(map(lambda c : 1 if password.letter in c else 0, password.password))
+    return password.min <= count and password.max >= count
 
-class Policy2():
-    def apply(self, password):
-        return (password.password[password.min - 1] == password.letter) ^ (password.password[password.max - 1] == password.letter)
+def policy2(password):
+    return (password.password[password.min - 1] == password.letter) ^ (password.password[password.max - 1] == password.letter)
 
 class Password():
     def __init__(self, line):
-        self.str = line
-        self.parse()
-
-    def parse(self):
-        str_min = self.str.split("-")[0]
-        str_max = self.str[len(str_min)+1:].split(" ")[0]
-        str_letter = self.str[len(str_min)+1 + len(str_max)+1:].split(":")[0]
+        str_min = line.split("-")[0]
+        str_max = line[len(str_min)+1:].split(" ")[0]
+        str_letter = line[len(str_min)+1 + len(str_max)+1:].split(":")[0]
         self.min = int(str_min)
         self.max = int(str_max)
         self.letter = str_letter
-        self.password = self.str[len(str_min) +  1 + len(str_max) + 1 + len(str_letter) + 1 + 1:]
+        self.password = line[len(str_min) +  1 + len(str_max) + 1 + len(str_letter) + 1 + 1:]
 
 def main():
     with open('input.txt', 'r') as f:
         lines = f.readlines()
         passwords = [Password(line) for line in lines]
-        count = len(list(filter(lambda password: Policy1().apply(password), passwords)))
-        print(count)
-        count = len(list(filter(lambda password: Policy2().apply(password), passwords)))
-        print(count)
+        count = len(list(filter(lambda password: policy1(password), passwords)))
+        print("Part One: {}".format(count))
+        count = len(list(filter(lambda password: policy2(password), passwords)))
+        print("Part Two: {}".format(count))
 
 if __name__ == "__main__":
     main()
