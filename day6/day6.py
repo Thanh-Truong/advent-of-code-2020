d45c959ad6ae16b85f9c3b891b09b227b53602a2
Thanh-Truong <hopchuathurac@gmail.com>
2020-12-07 21:33:44 +0100

    improved

--- day6/day6.py
+++ day6/day6.py
@@ -1,33 +1,31 @@
 def groups():
     with open('input.txt', 'r') as f:
-        lines = f.read().splitlines()
         group = []
-        for line in lines:
+        for line in f.read().splitlines():
             if line == "":
                 yield group
                 group.clear()
             else:
                 group.append(line)
-        if len(group) > 0:
-            yield group
+        yield group
 
-def countQuestionsAnyOneAnswerYes_2(group):
+def countNumberAnswers(group):
     answers = [0] * 26
     for person in group:
         for c in person:
-            answers[ord(c) - ord('a')] = 1 # there was an answer
-    return  sum(answers)
+            i = ord(c) - ord('a')
+            answers[i] = answers[i] +  1 # one more 'person' answered c
+    return answers
 
-def countQuestionsEveryOneAnswerYes_2(group):
-    answers = [0] * 26
-    size = len(group)
-    for person in group:
-        for c in person:
-            pos = ord(c) - ord('a')
-            answers[pos] = answers[pos] +  1 # this person answered
-    return  sum([1 for i in answers if i == size])
+def countQuestionsAnyOneAnswerYes(group):
+    answers = countNumberAnswers(group)
+    return sum([1 for i in answers if i > 0]) # anyone answered so i > 0
+
+def countQuestionsEveryOneAnswerYes(group):
+    answers = countNumberAnswers(group)
+    return  sum([1 for i in answers if i == len(group)]) # everyone answered so i == len(group)
 
 if __name__ == "__main__":
-    print(sum(map(lambda group: countQuestionsAnyOneAnswerYes_2(group), groups())))
-    print(sum(map(lambda group: countQuestionsEveryOneAnswerYes_2(group), groups())))
+    print(sum(map(lambda group: countQuestionsAnyOneAnswerYes(group), groups())))
+    print(sum(map(lambda group: countQuestionsEveryOneAnswerYes(group), groups())))
     
