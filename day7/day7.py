307a3e2f2547cab6c5d3bd36ed00ac83229d384e
Thanh-Truong <hopchuathurac@gmail.com>
2020-12-07 14:16:19 +0100

    Day7.Part Two

--- day7/day7.py
+++ day7/day7.py
@@ -13,21 +13,31 @@ def buildBags():
                if elements[0] != "no":
                    quantity = elements[0]
                    bag = " ".join(elements[1:])
-                   bags[outerBag].append((quantity, bag))
+                   bags[outerBag].append((int(quantity), bag))
     return bags
 
 def containGold(outerBag, bags):
     innerBags = bags[outerBag]
-    directlyFound = any(filter(lambda info: info[1] == 'shiny gold', innerBags))
-    if directlyFound:
-        return True
-    else:
-        return any(map(lambda bag: containGold(bag[1], bags), innerBags))
+    # directly or indirectly
+    return any(filter(lambda info: info[1] == 'shiny gold', innerBags)) or any(
+        map(lambda bag: containGold(bag[1], bags), innerBags))
+
+def countContainedBags(outerBag, bags):
+    innerBags = bags[outerBag]
+    return sum(map(
+        lambda bagInfo: bagInfo[0] + bagInfo[0] * countContainedBags(
+            bagInfo[1], bags), innerBags))
+
 if __name__ == "__main__":
     #rule = "light red bags contain 3 wavy teal bags, 3 plaid aqua bags, 4 drab lavender bags, 2 bright coral bags."
     bags = buildBags()
     count = 0
     for outerBag in bags.keys():
+        # PartOne
         if containGold(outerBag, bags):
             count = count + 1
-    pprint.pprint(count)
+    # PartTwo
+    aa = countContainedBags('shiny gold', bags)
+    
+    print("PartOne : {}".format(count))
+    print("PartTwo : {}".format(aa))
