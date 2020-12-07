893e4c0343f35b01904707432c66339226f18c03
Thanh-Truong <hopchuathurac@gmail.com>
2020-12-07 23:44:42 +0100

    cleaned up

--- day1/day1.py
+++ day1/day1.py
@@ -1,14 +1,8 @@
 
 import datetime
 import copy
-''' 
-567171
-----------------
-212428694
 
-'''
-
-def quicker():
+def main():
     with open('input.txt', 'r') as f:
         lines = f.read().splitlines()
         numbers = list(map(lambda line: int(line), lines))
@@ -24,24 +18,8 @@ def quicker():
                     if numbers[i] + numbers[j] + numbers[k]== 2020:
                         print("Part Two : {}".format(numbers[i]*numbers[j]*numbers[k]))
                         break
-        
-
-    """ size = len(listNumbers)
-    # Part 1
-    for i in range(size):
-        for j in range(size - i - 1):
-            if listNumbers[i] + listNumbers[j]== 2020:
-                print(listNumbers[i]*listNumbers[j])
-    print("----------------")
-    # Part 2
-    for i in range(size):
-        for j in range(size - i - 1):
-            for k in range(size - i - j - 1):
-                if listNumbers[i] + listNumbers[j] + listNumbers[k]== 2020:
-                    print(listNumbers[i]*listNumbers[j]*listNumbers[k])
-        
- """
+ 
     
 
 if __name__ == "__main__":
-    quicker()
+    main()
