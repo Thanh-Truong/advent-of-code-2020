5e11d3ae2fe22ed9baa12429396133067a9c1a8d
Thanh-Truong <hopchuathurac@gmail.com>
2020-12-07 15:56:07 +0100

    cleaned

--- day3/day3.py
+++ day3/day3.py
@@ -1,34 +1,26 @@
 def makeArray():
     with open('input.txt', 'r') as f:
-        lines = f.read().splitlines()
-        array = []
-        for line in lines:
-            array.append(line)
-    return array
+        return f.read().splitlines()
 
-
-def countTreesWithSlope(array, right, down):
-    countTrees = 0
-    col = 0
+def countTreesWithSlope(array, right, step):
+    count = 0
     row = 0
     while row < len(array):
-        col = row * right
-        checkPoint = col % len(array[row])
         if row != 0:  # start position
+            checkPoint = row * right % len(array[row])
             if array[row][checkPoint] == '#':
-                countTrees = countTrees + 1
-        #print("row = {} col = {} checkPoint = {} value = {} trees = {}".format(row, col, checkPoint, array[row][checkPoint], countTrees))
-        row = row + down
-    return countTrees
+                count = count + 1
+        row = row + step
+    return count
 
 
 def main():
     array = makeArray()
     # Part 1
-    print(countTreesWithSlope(array, right=3, down=1))
+    print(countTreesWithSlope(array, right=3, step=1))
     # Part 2
-    result = countTreesWithSlope(array, right=1, down=1) * countTreesWithSlope(array, right=3, down=1) * countTreesWithSlope(
-        array, right=5, down=1) * countTreesWithSlope(array, right=7, down=1) * countTreesWithSlope(array, right=1, down=2)
+    result = countTreesWithSlope(array, right=1, step=1) * countTreesWithSlope(array, right=3, step=1) * countTreesWithSlope(
+        array, right=5, step=1) * countTreesWithSlope(array, right=7, step=1) * countTreesWithSlope(array, right=1, step=2)
     print(result)
 
 
