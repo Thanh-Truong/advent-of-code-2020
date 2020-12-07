02bcf786e7acd22ba2bf297cf4d108c786e46f72
Thanh-Truong <hopchuathurac@gmail.com>
2020-12-07 23:44:10 +0100

    cleaned up

--- day1/day1.py
+++ day1/day1.py
@@ -1,62 +1,32 @@
 
 import datetime
 import copy
+''' 
+567171
+----------------
+212428694
 
-CURRENT_YEAR = datetime.date.today().year
-YEAR = [-1] * (CURRENT_YEAR + 1)
-    
-def exists(i, array):
-    return array[i] == i
-
-def findPairsOfNumbers(array, sum):
-    # Go through the YEAR array and sum up
-    length = len(array)
-    for i in range(0, length):
-        if exists(i, array):
-            j = sum - i
-            if exists(j, array):
-                yield(i, j)
-                array[j] = - 1
-        array[i] = -1
-        
-
-def findTripleOfNumbers(array, sum):
-    # Go through the YEAR array and sum up
-    for i in range(0, len(array)):
-        if exists(i, array):
-            sub_sum = sum - i
-            for j, k in findPairsOfNumbers(array[0: sub_sum], sub_sum):
-                print("found {} {} {} {}".format(i, j, k, sub_sum))
-                yield(i, j, k)
-
-def main():
-    # Fill in the YEAR array with input values
-    with open('input.txt', 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            i = int(line)
-            if i > CURRENT_YEAR:
-                raise ValueError("Input value is greater than {}".format(CURRENT_YEAR))
-            YEAR[i] = i
-
-    array = copy.copy(YEAR)
-    # Go through the YEAR array and sum up
-    for i, j in findPairsOfNumbers(array[0: CURRENT_YEAR], CURRENT_YEAR):
-        print(i*j)
-    
-    array = copy.copy(YEAR)
-    for i, j, k in findTripleOfNumbers(array, CURRENT_YEAR):
-       print(i*j*k)
+'''
 
 def quicker():
-    # Fill in the YEAR array with input values
-    listNumbers = []
     with open('input.txt', 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            listNumbers.append(int(line))
+        lines = f.read().splitlines()
+        numbers = list(map(lambda line: int(line), lines))
+        for i in numbers:
+            if (2020 - i) in numbers:
+                print("Part One : {}".format((2020-i)*i))
+                break
+        
+        size = len(numbers)
+        for i in range(size - 2):
+            for j in range(i, size - 1):
+                for k in range(j, size):
+                    if numbers[i] + numbers[j] + numbers[k]== 2020:
+                        print("Part Two : {}".format(numbers[i]*numbers[j]*numbers[k]))
+                        break
+        
 
-    size = len(listNumbers)
+    """ size = len(listNumbers)
     # Part 1
     for i in range(size):
         for j in range(size - i - 1):
@@ -70,9 +40,8 @@ def quicker():
                 if listNumbers[i] + listNumbers[j] + listNumbers[k]== 2020:
                     print(listNumbers[i]*listNumbers[j]*listNumbers[k])
         
-
+ """
     
 
 if __name__ == "__main__":
-    main()
     quicker()
