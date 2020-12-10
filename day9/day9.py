8ae149bcbe9e01fa44bbd9c05d2c8a2e33d6d853
Thanh-Truong <hopchuathurac@gmail.com>
2020-12-10 02:21:05 +0100

    Day9. Improved

--- day9/day9.py
+++ day9/day9.py
@@ -18,20 +18,14 @@ def findContiguousSetWithSumEquals(value,numbers):
     # Find contiguous set of at least two numbers that sums up to 'firstInvalidNum'
     start = 0
     stop = 0
-    for i in range(len(numbers)):
-        #print("{} current {} start {} stop {}".format(numbers[start:stop], numbers[i], start, stop))
-        total = sum(numbers[start:stop])
+    for _ in range(len(numbers)):
+        print("{} current {} start {} stop {}".format(numbers[start:stop], numbers[_], start, stop))
+        # expand first
+        stop +=1
+        while sum(numbers[start:stop]) > firstInvalidNumber:
+            # throw out elements from the left if sum is bigger
+            start = start + 1
 
-        # expand
-        if total + numbers[i] < firstInvalidNumber:
-            stop += 1
-        elif total + numbers[i] >= firstInvalidNumber:
-            stop += 1
-            while sum(numbers[start:stop]) > firstInvalidNumber:
-                # throw out elements from the left
-                start = start + 1
-
-        # newTotal == firstInvalidNumber
         if sum(numbers[start:stop]) == firstInvalidNumber and twoContiguousNumbers(numbers[start:stop]):
             return numbers[start:stop]
 
